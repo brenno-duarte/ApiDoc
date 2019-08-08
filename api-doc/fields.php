ab3de6476a9adb5dced7b48a171351355b7b263f
brennoDuarte <brenno.gnr@gmail.com>
2019-08-08 19:55:39 -0300

    fix bug in index.php

--- api-doc/fields.php
+++ api-doc/fields.php
@@ -91,7 +91,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"><?= $r->get->pathGet; ?></h5>
+                    <h5 class="modal-title" id="exampleModalLabel"><?= $r->getId->pathGetId; ?></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
                     <span aria-hidden="true">&times;</span>
                     </button>
--- api-doc/index.php
+++ api-doc/index.php
@@ -7,7 +7,13 @@ $info = $api->getInfo();
 $router = $api->getRouter();
 
 foreach ($router->path as $value) {
-    if ($value->get->pathGet == NULL || $value->post->pathPost == NULL || $value->put->pathPut == NULL || $value->delete->pathDelete == NULL) {
+    if ($value->get->pathGet == NULL || $value->getId->pathGetId == NULL || $value->post->pathPost == NULL 
+    || $value->put->pathPut == NULL || $value->delete->pathDelete == NULL || 
+    $value->get->descriptionGet == NULL || $value->getId->descriptionGetId == NULL ||
+    $value->post->descriptionPost == NULL || $value->put->descriptionPut == NULL || 
+    $value->delete->descriptionDelete == NULL || $value->get->link == NULL || 
+    $value->getId->link == NULL || $value->post->link == NULL || 
+    $value->put->link == NULL || $value->delete->link == NULL) {
         $warning = "There are some null fields in your json. Some routes will be empty";
     }
 }
