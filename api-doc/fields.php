aea00c0748b9daf513fdbab34082a59d3fbb0797
brennoDuarte <brenno.gnr@gmail.com>
2019-11-11 18:39:37 -0300

    backup

--- api-doc/fields.php
+++ api-doc/fields.php
@@ -47,7 +47,6 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="CustomPaths">Custom Paths</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
                         <span aria-hidden="true">&times;</span>
                         </button>
